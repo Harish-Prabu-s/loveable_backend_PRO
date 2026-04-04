b31fba06da100a8e7cbcdfc0e948887ef7fceb6f
Maverick <macerick444@gmail.com>
2026-04-04 19:48:54 +0530

    games

--- api/modules/games/urls.py
+++ api/modules/games/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .controllers import list_games_view, icebreaker_prompt_view, create_game_room_view, matchmake_view
+from .controllers import (
+    list_games_view, icebreaker_prompt_view, create_game_room_view, 
+    matchmake_view, leave_matchmake_view, send_invite_view, respond_invite_view
+)
 
 urlpatterns = [
     path('', list_games_view),
     path('icebreaker/<str:kind>/', icebreaker_prompt_view),
-    path('create/', create_game_room_view),
+    path('create-room/', create_game_room_view),
     path('matchmake/', matchmake_view),
+    path('matchmake/leave/', leave_matchmake_view),
+    path('invite/', send_invite_view),
+    path('invite/respond/', respond_invite_view),
 ]
