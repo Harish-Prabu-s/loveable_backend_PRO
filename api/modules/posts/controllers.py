ecc45c835e0ac97c49d75930e49d9448e0535909
Maverick <macerick444@gmail.com>
2026-08-18 17:19:55 +0530

    ok

--- api/modules/posts/controllers.py
+++ api/modules/posts/controllers.py
@@ -208,7 +208,6 @@ def create_post_view(request):
         
         if hashtags:
             # Merge hashtags into caption or just sync them
-            from ..hashtags.controllers import sync_hashtags
             full_text = caption + " " + " ".join([f"#{h.lstrip('#')}" for h in hashtags])
             sync_hashtags(full_text, post)
         else:
